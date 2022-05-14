69f254d66ab808f409fc421384af3e5bf9232401
Malte Bartels <malte.bartels@live.de>
2022-05-14 16:59:45 +0200

    Add diagram

--- src/lib/confusion-diagram.svelte
+++ src/lib/confusion-diagram.svelte
@@ -1,8 +1,48 @@
 <script>
-	/** @type {{time: Number, value: Boolean}[]} */
+	import { LayerCake, Svg } from 'layercake';
+
+	import Line from '$lib/Line.svelte';
+	import Area from '$lib/Area.svelte';
+	import AxisX from '$lib/AxisX.svelte';
+	import AxisY from '$lib/AxisY.svelte';
+
+	const X_KEY = 'time';
+	const Y_KEY = 'value';
+
+	/** @type {{time: Number, value: Number}[]} */
 	export let data = [];
+
+	data.forEach((d) => {
+		d[Y_KEY] = +d[Y_KEY];
+	});
 </script>
 
-<h2 class="text-xl md:text-2xl font-bold font-serif">Current class confusion:</h2>
-<p>Diagram goes here</p>
-<p>{data}</p>
+<div class="chart-container">
+	<LayerCake
+		padding={{ right: 10, bottom: 20, left: 25 }}
+		x={X_KEY}
+		y={Y_KEY}
+		yDomain={[0, null]}
+		{data}
+	>
+		<Svg>
+			<AxisX />
+			<AxisY ticks={4} />
+			<Line />
+			<Area />
+		</Svg>
+	</LayerCake>
+</div>
+
+<style>
+	/*
+	  The wrapper div needs to have an explicit width and height in CSS.
+	  It can also be a flexbox child or CSS grid element.
+	  The point being it needs dimensions since the <LayerCake> element will
+	  expand to fill it.
+	*/
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- src/routes/[roomNumber]/student.svelte
+++ src/routes/[roomNumber]/student.svelte
@@ -11,6 +11,29 @@
 	let isLost = false;
 	const setStatusLost = () => (isLost = true);
 	const setStatusNotLost = () => (isLost = false);
+
+	const MOCK_DATA = [
+		{ time: 1, value: 0.1 },
+		{ time: 2, value: 0.3 },
+		{ time: 3, value: 0.2 },
+		{ time: 4, value: 0.3 },
+		{ time: 5, value: 0.4 },
+		{ time: 6, value: 0.6 },
+		{ time: 7, value: 0.7 },
+		{ time: 8, value: 0.75 },
+		{ time: 9, value: 0.78 },
+		{ time: 10, value: 0.8 },
+		{ time: 11, value: 0.9 },
+		{ time: 12, value: 0.96 },
+		{ time: 13, value: 0.8 },
+		{ time: 14, value: 0.7 },
+		{ time: 15, value: 0.8 },
+		{ time: 16, value: 0.86 },
+		{ time: 17, value: 0.78 },
+		{ time: 18, value: 0.89 },
+		{ time: 19, value: 0.9 },
+		{ time: 20, value: 0.95 }
+	];
 </script>
 
 <main class="grid sm:grid-cols-2 lg:grid-cols-3 gap-3 p-5">
@@ -37,8 +60,10 @@
 			</div>
 		</div>
 	</div>
-	<div class="bg-white p-5 sm:p-10 md:p-15">
-		<ConfusionDiagram data={[]} />
+	<div class="bg-white p-5 sm:p-10 md:p-15 min-h-[50vh] sm:min-h-0 flex flex-col">
+		<h2 class="text-lg md:text-2xl font-bold font-serif mb-4">Current class confusion:</h2>
+
+		<ConfusionDiagram data={MOCK_DATA} />
 	</div>
 	<div class="bg-white p-5 sm:p-10 md:p-15 col-start-1">
 		<h2 class="text-lg md:text-2xl font-bold font-serif mb-4">Everything done here?</h2>
