ac3358614788b6aa1f41e587c3cc65bd28825269
Malte Bartels <malte.bartels@live.de>
2022-05-14 17:22:28 +0200

    Add not found page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,9 @@
+<main>
+	<div class="shadow bg-white m-5 flex flex-col items-center md:max-w-2xl md:mx-auto p-10 md:p-15">
+		<h1 class="text-3xl md:text-6xl font-serif font-extrabold mb-10">404 Not Found</h1>
+		<p class="max-w-xs">
+			Oh no, you got lost! This page doesn't exist. Maybe you entered an incorrect room number?
+		</p>
+		<a class="button block mt-5" href="/">&blacktriangleleft; Back to start</a>
+	</div>
+</main>
