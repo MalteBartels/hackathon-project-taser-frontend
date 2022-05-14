bae842628aa8a7866f182e1df75ee57d44d08421
Malte Bartels <malte.bartels@live.de>
2022-05-14 15:30:55 +0200

    Add login components

---
+++ src/lib/login.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	let roomNumber = '';
+
+	function handleLogin() {
+		dispatch('login', {
+			roomNumber
+		});
+	}
+</script>
+
+<div class="grid items-center">
+	<h1 class="text-3xl font-serif font-extrabold mb-4">Enter your Room Number:</h1>
+	<div class="flex flex-col sm:flex-row items-center sm:items-stretch">
+		<input
+			bind:value={roomNumber}
+			class="sm:flex-grow p-3 border-2 border-slate-900 focus:outline-none focus:ring-4 focus:ring-slate-400 sm:border-r-0"
+			type="text"
+			placeholder="e.g. UN38-04"
+		/>
+		<button on:click={handleLogin} class="button button--solid mt-1 sm:mt-0"
+			>Join &blacktriangleright;</button
+		>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,65 @@
 <script>
 	import LandingPeeps from '$lib/landing-peeps.svelte';
 	import Layout from './__layout.svelte';
-	import { fade } from 'svelte/transition';
-	import { onMount } from 'svelte';
+	import Login from '$lib/login.svelte';
 
-	let mounted = false;
-	onMount(() => (mounted = true));
+	// Constants to avoid typos in identifying strings
+	const STUDENT = 'student';
+	const TEACHER = 'teacher';
+	const ADMIN = 'admin';
+
+	/**
+	 * Contains as which user we should log in
+	 * @type {"student" | "teacher" | "admin" | ''}
+	 */
+	let loginAsRole = '';
+	const loginAsStudent = () => (loginAsRole = STUDENT);
+	const loginAsTeacher = () => (loginAsRole = TEACHER);
+	const loginAsAdmin = () => (loginAsRole = ADMIN);
+	const resetLogin = () => (loginAsRole = '');
+
+	/**
+	 * Handles the login event of the login component
+	 *
+	 * @param {{ detail: {roomNumber: Number}}} event
+	 */
+	function handleLogin(event) {
+		alert(event.detail.roomNumber);
+	}
 </script>
 
 <Layout>
 	<main>
-		{#if mounted}
-			<div transition:fade class="shadow bg-white m-5 flex flex-col md:max-w-2xl md:mx-auto">
-				<div class="bg-slate-100 md:p-5">
-					<LandingPeeps />
-				</div>
-				<div class="p-10 md:p-15">
-					<h1 class="text-3xl text-6xl font-serif font-extrabold text-center mb-10">
+		<div class="shadow bg-white m-5 flex flex-col md:max-w-2xl md:mx-auto">
+			<div class="bg-slate-100 md:p-5">
+				<LandingPeeps />
+			</div>
+			<div class="p-10 md:p-15">
+				{#if loginAsRole}
+					<button class="font-bold" on:click={resetLogin}>&blacktriangleleft; back</button>
+				{/if}
+				{#if !loginAsRole}
+					<h1 class="text-3xl md:text-6xl font-serif font-extrabold text-center mb-10">
 						Welcome to Project Taser!
 					</h1>
 					<nav
 						class="flex flex-col sm:flex-row justify-center items-center space-y-3 sm:space-y-0 sm:space-x-3"
 					>
-						<a class="button" href="/student">Student</a>
-						<a class="button " href="/teacher">Teacher</a>
-						<a class="button" href="/admin">Admin</a>
+						<button class="button" on:click={loginAsStudent}>Student</button>
+						<button class="button" on:click={loginAsTeacher}>Teacher</button>
+						<button class="button" on:click={loginAsAdmin}>Admin</button>
 					</nav>
-				</div>
+				{:else if loginAsRole === STUDENT || loginAsRole === TEACHER}
+					<div class="mt-4">
+						<Login on:login={handleLogin} />
+					</div>
+				{:else if loginAsRole === ADMIN}
+					<div class="flex flex-col items-center">
+						<p class="mb-3">TODO: Implement authentication for admins</p>
+						<a class="button button-solid" href="/teacher">Login</a>
+					</div>
+				{/if}
 			</div>
-		{/if}
+		</div>
 	</main>
 </Layout>
