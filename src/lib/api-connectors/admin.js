1799cfae598d5259cc5622e435d5ba0979c14fb1
Malte Bartels <malte.bartels@live.de>
2022-05-15 12:22:29 +0200

    Add admin methods

--- src/lib/api-connectors/admin.js
+++ src/lib/api-connectors/admin.js
@@ -1,14 +1,16 @@
+import { ADMIN_API_URL } from "$lib/variables";
+
 const MOCK_ROOMS = ['U38-01.01', 'U38-01.02', 'U38-01.03', 'U38-01.04'];
 
 /**
  * @returns String[]
  */
 export const getAllRooms = async () => {
-    return MOCK_ROOMS;
+    const response = await fetch(`${ADMIN_API_URL}/rooms`).then((response) => response.json())
+    return response.map((/** @type {{roomId: String}}*/ {roomId}) => roomId)
 }
 
 /**
- * @param String room 
  * @returns String[]
  */
 export const removeRoom = async (/** @type String*/ room) => {
@@ -17,10 +19,15 @@ export const removeRoom = async (/** @type String*/ room) => {
 }
 
 /**
- * @param String room 
  * @returns String[]
  */
-export const addRoom = async (/** @type String*/ room) => {
-    MOCK_ROOMS.push(room);
-    return MOCK_ROOMS;
+export const addRoom = async (/** @type String*/ roomNumber) => {
+    const response = await fetch(`${ADMIN_API_URL}/room/create/${encodeURIComponent(roomNumber)}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({}) // body data type must match "Content-Type" header
+      }).then((response) => response.json());
+    return response.map((/** @type {{roomId: String}}*/ {roomId}) => roomId)
 }
--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,2 +1,3 @@
 export const QUEUE_API_URL = import.meta.env.VITE_QUEUE_API_URL
 export const REAL_TIME_API_URL = import.meta.env.VITE_REAL_TIME_API_URL
+export const ADMIN_API_URL = import.meta.env.VITE_ADMIN_API_URL
